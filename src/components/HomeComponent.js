1d094196ad80d74f0c9e95623d26e48b4cc91bf0
Tayomide <tibrahim@hawk.iit.edu>
2022-06-27 00:57:59 -0500

    Structure HTML layout

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -9,6 +9,20 @@ export const HomeComponent = () => {
             <source srcSet="/M3-Homepage-Mobile.jpg" />
             <img src="small.jpg" alt="The president giving an award." />
         </picture>
+        <HomeStatic>
+            <h1>Model 3</h1>
+            <p>Order Online for <Link to="/">Touchless Delivery</Link></p>
+            <ul>
+                <li>
+                    <Link to="/">Custom Order</Link>
+                </li>
+                <li>
+                    <Link to="/">Existing Inventory</Link>
+                </li>
+            </ul>
+            <p></p>
+            <button></button>
+        </HomeStatic>
     </HomeComponentContainer>
   )
 }
