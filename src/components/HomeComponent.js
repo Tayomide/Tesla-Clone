3c6120cd09b9a72bd3913faaee8e85faa1aabdc2
Tayomide <tibrahim@hawk.iit.edu>
2022-06-26 23:40:36 -0500

    Create reusable component for home page

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const HomeComponent = () => {
+  return (
+    <HomeComponentContainer>
+        HomeComponent
+    </HomeComponentContainer>
+  )
+}
+
+const HomeComponentContainer = styled.div``
