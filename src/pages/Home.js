e7392d9b45024381549c2634fa2b702807d39e8c
Tayomide <tibrahim@hawk.iit.edu>
2022-06-27 02:11:34 -0500

    Add all components of Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,58 @@ export const Home = () => {
           desktopImage = "/M3-Homepage-Desktop.jpg"
           altText = "Model 3"
           title = "Model 3"
+          leftButtonText = "Custom Order"
+          rightButtonText = "Existing Inventory"
+        />
+        <HomeComponent
+          mobileImage = "/MY-Homepage-Mobile.jpg"
+          desktopImage = "/MY-Homepage-Desktop.jpg"
+          altText = "Model Y"
+          title = "Model Y"
+          leftButtonText = "Custom Order"
+          rightButtonText = "Existing Inventory"
+        />
+        <HomeComponent
+          mobileImage = "/MX-Homepage-Mobile.jpg"
+          desktopImage = "/MX-Homepage-Desktop.jpg"
+          altText = "Model X"
+          title = "Model X"
+          leftButtonText = "Custom Order"
+          rightButtonText = "Existing Inventory"
+        />
+        <HomeComponent
+          mobileImage = "/MS-Homepage-Mobile.jpg"
+          desktopImage = "/MS-Homepage-Desktop.jpg"
+          altText = "Model S"
+          title = "Model S"
+          leftButtonText = "Custom Order"
+          rightButtonText = "Existing Inventory"
+        />
+        <HomeComponent
+          mobileImage = "/SP-Homepage-Mobile.jpg"
+          desktopImage = "/SP-Homepage-Desktop.jpg"
+          altText = "Solar Panels"
+          title = "Solar Panels"
+          leftButtonText = "Order Now"
+          rightButtonText = "Learn More"
+          description = "Lowest Cost Solar Panels in America"
+        />
+        <HomeComponent
+          mobileImage = "/SR-Homepage-Mobile.jpg"
+          desktopImage = "/SR-Homepage-Desktop.jpg"
+          altText = "Solar Roof"
+          title = "Solar Roof"
+          leftButtonText = "Order Now"
+          rightButtonText = "Learn More"
+          description = "Produce Clean Energy From Your Roof"
+        />
+        <HomeComponent
+          mobileImage = "/AS-Homepage-Mobile.jpg"
+          desktopImage = "/AS-Homepage-Desktop.jpg"
+          altText = "Accessories"
+          title = "Accessories"
+          leftButtonText = "Shop Now"
+          description = ""
         />
     </HomeContainer>
   )
