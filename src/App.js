f92c3c1c03a6d67e1c41e94f242aca6594d5c0cd
Tayomide <tibrahim@hawk.iit.edu>
2022-06-26 23:31:36 -0500

    Add routing structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import styled from "styled-components"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <AppContainer>
-      
+      <Router>
+        <Routes>
+        </Routes>
+      </Router>
     </AppContainer>
   )
 }
