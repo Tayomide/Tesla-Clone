d96ed2e550123b9a6475afd488d25046fd026d9d
Tayomide <tibrahim@hawk.iit.edu>
2022-06-27 01:46:27 -0500

    Style reusable Home component

---
+++ src/components/Home.js
@@ -0,0 +1,100 @@
+import styled from "styled-components"
+import { Link } from "react-router-dom"
+import { TeslaButton } from "./TeslaButton"
+
+export const HomeComponent = () => {
+  return (
+    <HomeComponentContainer>
+        <picture>
+            <source media="(min-width: 600px)" srcSet="/M3-Homepage-Desktop.jpg"/>
+            <source srcSet="/M3-Homepage-Mobile.jpg" />
+            <img src="small.jpg" alt="The president giving an award." />
+        </picture>
+        <HomeStatic>
+            <h1>Model 3</h1>
+            <p>Order Online for <Link to="/">touchless delivery</Link></p>
+            <ul>
+                <li>
+                    <TeslaButton 
+                        content = "Custom Order"
+                        background = "#171a20cc"
+                        border = "transparent"
+                        color = "#fff"
+                    />
+                </li>
+                <li>
+                <TeslaButton 
+                        content = "Existing Inventory"
+                        background = "#f4f4f4a6"
+                        border = "transparent"
+                        color = "#393c41"
+                    />
+                </li>
+            </ul>
+            <p></p>
+            <button></button>
+        </HomeStatic>
+    </HomeComponentContainer>
+  )
+}
+
+const HomeComponentContainer = styled.div`
+    height: 100vh;
+    position: relative;
+    overflow: hidden;
+    picture{
+        width: 100%;
+        height: 100%;
+        img{
+            object-fit: cover;
+            width: 100%;
+            height: 100%;
+        }
+    }
+`
+
+const HomeStatic = styled.div`
+    position: sticky;
+    bottom: 0;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 24px;
+    align-items: center;
+    h1{
+        margin-top: calc(18vh - 52px);
+    }
+    h1 + p{
+        flex: 1;
+        padding: 6px 24px 0 24px;
+        a{
+            text-transform: capitalize;
+            border-bottom: 1px solid black;
+        }
+    }
+    ul{
+        padding: 16px 0;
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+        li{
+            width: 100%;
+            height: 40px;
+            display: flex;
+            flex-direction: row;
+        }
+    }
+    ul + p {
+        font-size: 14px;
+        line-height: 18px;
+        height: 30px;
+        width: 100%;
+        padding: 0 24px 12px 24px;
+    }
+    button {
+        height: 24px;
+        width: 24px;
+    }
+`
