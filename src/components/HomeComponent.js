7472a44d139356ef96bcb44e91cb29f8f2eb4024
Tayomide <tibrahim@hawk.iit.edu>
2022-06-27 00:32:40 -0500

    Add responsive image for home component

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,7 +3,11 @@ import styled from "styled-components"
 export const HomeComponent = () => {
   return (
     <HomeComponentContainer>
-        HomeComponent
+        <picture>
+            <source media="(min-width: 600px)" srcSet="/M3-Homepage-Desktop.jpg"/>
+            <source srcSet="/M3-Homepage-Mobile.jpg" />
+            <img src="small.jpg" alt="The president giving an award." />
+        </picture>
     </HomeComponentContainer>
   )
 }
